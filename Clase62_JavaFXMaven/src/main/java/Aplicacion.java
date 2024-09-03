dfc635232e54880807abc67089c2e9a8ff9704f2
safaren <sergioaparicio@gmail.com>
2024-09-03 10:19:39 +0200

    Añadido FX normal y maven

---
+++ Clase62_JavaFXMaven/src/main/java/Aplicacion.java
@@ -0,0 +1,42 @@
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Aplicacion extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		Label label = new Label("Hello");
+		TextField textField = new TextField();
+		Button button = new Button("Aceptar");
+		Label label2 = new Label("Qué pasaaa!!");
+		
+		// Layout vertical
+		VBox root = new VBox(label, textField, button);
+		
+		// panel, layout
+		Scene scene = new Scene(new VBox(label, textField, button), 200, 200);
+		
+		// Añadir más componentes despues de la creación del root
+		root.getChildren().add(label2);
+		
+		button.setOnAction(e -> {
+			label2.setText(textField.getText());
+		});
+		
+		//Ventana
+		primaryStage.setScene(scene);
+		//Titulo
+		primaryStage.setTitle("Primer EFX");
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
